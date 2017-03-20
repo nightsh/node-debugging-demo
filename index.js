9a17711e8a5be78a27919e7bd555617581f86987
Victor Nițu <victor@thesponge.eu>
2017-03-20 13:52:24 +0200

    Semicolons everywhere!

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
 app.get('/', function (req, res) {
   debugger;
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+  console.log('Example app listening on port 3000!');
+});
